3b176645e255b2253e14169b64bfe274d32f76f3
Pablo Biagioli <pabiagioli@gmail.com>
2016-07-20 14:14:21 -0300

    added WoL logic, status codes and WoL test as dependency

--- app/src/test/java/com/aajtech/mobile/goproshowcase/GoProCommandsUnitTest.kt
+++ app/src/test/java/com/aajtech/mobile/goproshowcase/GoProCommandsUnitTest.kt
@@ -1,10 +1,12 @@
 package com.aajtech.mobile.goproshowcase
 
+import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import org.junit.Test
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.concurrent.TimeUnit
@@ -22,37 +24,78 @@ class GoProCommandsUnitTest {
             .build()!!
     }
 
+    val moshi: Moshi by lazy { Moshi.Builder().build()!! }
+
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.5.5.9/gp/gpControl/")
-            .client(okHttpClient)
-            .build()!!
+                .baseUrl("http://10.5.5.9/gp/gpControl/")
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()!!
     }
 
     @Test
+    fun testWoL(){
+        val macStr = "F4DD9E0DE0F7"
+        val ipStr = "10.5.5.9"
+        try	{
+            val macStr1 = MagicPacket.cleanMac(macStr)
+            println("Sending to: "+macStr)
+            MagicPacket.send(macStr1, ipStr)
+        }
+        catch(e:IllegalArgumentException) {
+            e.printStackTrace()
+        }catch(e:Exception) {
+            System.out.println("Failed to send Wake-on-LAN packet:" + e.message)
+        }
+    }
+
+    /**
+     * "info": {
+     * "model_number": 16,
+     * "model_name": "HERO4 Session",
+     * "firmware_version": "HX1.01.01.00",
+     * "serial_number": "C3141324949509",
+     * "board_type": "0x07",
+     * "ap_mac": "F4DD9E0DE0F7",
+     * "ap_ssid": "aajGoPro",
+     * "ap_has_default_credentials": "0",
+     * "git_sha1": "e4c3eabd3ab8d3f53067e986458ef04652222fd4"
+     * }
+     */
+    @Test()
     fun testGoProInfo(){
         val ssid = "aajGoPro"
         val pass = "44JT3ch01"
+
+        testWoL()
+
         val service = retrofit.create(GoProInfoService::class.java)
 
         //Requests
-        val nameCmdReq = service.nameCmd(ssid,pass)
+        //val nameCmdReq = service.nameCmd(ssid,pass)
         val statusReq = service.status()
 
         //Responses
-        val responsePair = nameCmdReq.execute()
+        //val responsePair = nameCmdReq.execute()
         val responseStatus = statusReq.execute()
 
 
-        assert(responsePair.isSuccessful)
-        println(responsePair.body().string())
+        //assert(responsePair.isSuccessful)
+        //println(responsePair.body().string())
 
         assert(responseStatus.isSuccessful)
-        println(responseStatus.body().string())
+        println(responseStatus.body().toString())
+        val batLvlK = GoProConstants.status[GoProConstants.batteryLevel]!!
+        val batLvl = responseStatus.body()["status"]!![batLvlK]
+        println("Battery Level $batLvlK : $batLvl")
     }
 
     @Test
     fun testGetAnalytics(){
+
+        testWoL()
+
         val service = retrofit.create(GoProAnalyticsService::class.java)
         assert(service.analytics().execute().isSuccessful)
         /*.enqueue(object : Callback<ResponseBody> {
@@ -69,12 +112,71 @@ class GoProCommandsUnitTest {
     }
 }
 
+object GoProConstants {
 
+    /**
+     * Go Pro Hero 4 Session's Internal Battery Level
+     */
+    val batteryLevel = "InternalBatteryLevel"
+
+    val status: Map<String, Int>
+        get() = mapOf(
+                "InternalBatteryPresent" to 1,
+                "InternalBatteryLevel" to 2,
+                "ExternalBatteryPresent" to 3,
+                "ExternalBatteryLevel" to 4,
+                "CurrentTemperature" to 5,
+                "SystemHot" to 6,
+                "SystemBusy" to 8,
+                "QuickCaptureActive" to 9,
+                "EncodingActive" to 10,
+                "LcdLockActive" to 11,
+                "CameraLocateActive" to 45,
+                "Mode" to 43,
+                "SubMode" to 44,
+                "Xmode" to 12,
+                "VideoProgressCounter" to 13,
+                "VideoProtuneDefault" to 46,
+                "PhotoProtuneDefault" to 47,
+                "MultiShotProtuneDefault" to 48,
+                "MultiShotCountDown" to 49,
+                "BroadcastProgressCounter" to 14,
+                "BroadcastViewersCount" to 15,
+                "BroadcastBstatus" to 16,
+                "WirelessEnable" to 17,
+                "WirelessPairState" to 19,
+                "WirelessPairType" to 20,
+                "WirelessScanState" to 22,
+                "WirelessScanTime" to 23,
+                "WirelessScanCurrentTime" to 18,
+                "WirelessPairing" to 28,
+                "WirelessRemoveControlVersion" to 26,
+                "WirelessRemoveControlConnected" to 27,
+                "WirelessAppCount" to 31,
+                "WirelessProvisionStatus" to 24,
+                "WirelessRssiBars" to 25,
+                "WirelessWlanSsid" to 29,
+                "WirelessApSsid" to 30,
+                "SdStatus" to 33,
+                "RemainingPhotos" to 34,
+                "RemainingVideoTime" to 35,
+                "NumGroupPhotos" to 36,
+                "NumGroupVideos" to 37,
+                "NumTotalPhotos" to 38,
+                "NumTotalVideos" to 39,
+                "DateTime" to 40,
+                "FWUpdateOtaStatus" to 41,
+                "FWUpdateDownloadCancelRequestPending" to 42
+        )
+}
 
 interface GoProInfoService {
 
+    /**
+     * get Status codes
+     */
     @GET("status")
-    fun status():Call<ResponseBody>
+    fun status():Call<Map<String,Map<Int,Any>>>
 
     @GET("command/wireless/ap/ssid")
     fun nameCmd(@Query("ssid")ssid:String, @Query("pw")pass:String): Call<ResponseBody>
@@ -88,3 +190,4 @@ interface GoProAnalyticsService {
     @GET("analytics/get")
     fun analytics():Call<ResponseBody>
 }
+
---
+++ app/src/test/java/com/aajtech/mobile/goproshowcase/MagicPacket.kt
@@ -0,0 +1,120 @@
+package com.aajtech.mobile.goproshowcase
+
+import java.io.IOException
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.SocketException
+import java.net.UnknownHostException
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ * Created by pablo.biagioli on 7/20/16.
+ */
+object MagicPacket {
+    private val TAG = "MagicPacket"
+
+    val BROADCAST = "192.168.1.255"
+    val PORT = 9
+    val SEPARATOR = ':'
+
+    @Throws(UnknownHostException::class, SocketException::class, IOException::class, IllegalArgumentException::class)
+    @JvmOverloads fun send(mac: String, ip: String, port: Int = PORT): String {
+        // validate MAC and chop into array
+        val hex = validateMac(mac)
+
+        // convert to base16 bytes
+        val macBytes = ByteArray(6)
+        for (i in 0..5) {
+            macBytes[i] = Integer.parseInt(hex[i], 16).toByte()
+        }
+
+        val bytes = ByteArray(102)
+
+        // fill first 6 bytes
+        for (i in 0..5) {
+            bytes[i] = 0xff.toByte()
+        }
+        // fill remaining bytes with target MAC
+        var i = 6
+        while (i < bytes.size) {
+            System.arraycopy(macBytes, 0, bytes, i, macBytes.size)
+            i += macBytes.size
+        }
+
+        // create socket to IP
+        val address = InetAddress.getByName(ip)
+        val packet = DatagramPacket(bytes, bytes.size, address, port)
+        val socket = DatagramSocket()
+        socket.send(packet)
+        socket.close()
+
+        return hex[0] + SEPARATOR + hex[1] + SEPARATOR + hex[2] + SEPARATOR + hex[3] + SEPARATOR + hex[4] + SEPARATOR + hex[5]
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun cleanMac(mac: String): String {
+        val hex = validateMac(mac)
+
+        var sb = StringBuffer()
+        var isMixedCase = false
+
+        // check for mixed case
+        for (i in 0..5) {
+            sb.append(hex[i])
+        }
+        val testMac = sb.toString()
+        if (testMac.toLowerCase() == testMac == false && testMac.toUpperCase() == testMac == false) {
+            isMixedCase = true
+        }
+
+        sb = StringBuffer()
+        for (i in 0..5) {
+            // convert mixed case to lower
+            if (isMixedCase == true) {
+                sb.append(hex[i].toLowerCase())
+            } else {
+                sb.append(hex[i])
+            }
+            if (i < 5) {
+                sb.append(SEPARATOR)
+            }
+        }
+        return sb.toString()
+    }
+
+    @Throws(IllegalArgumentException::class)
+    private fun validateMac(mac: String): Array<String> {
+        var mac = mac
+        // error handle semi colons
+        mac = mac.replace(";", ":")
+
+        // attempt to assist the user a little
+        var newMac = ""
+
+        if (mac.matches("([a-zA-Z0-9]){12}".toRegex())) {
+            // expand 12 chars into a valid mac address
+            for (i in 0..mac.length - 1) {
+                if (i > 1 && i % 2 == 0) {
+                    newMac += ":"
+                }
+                newMac += mac[i]
+            }
+        } else {
+            newMac = mac
+        }
+
+        // regexp pattern match a valid MAC address
+        val pat = Pattern.compile("((([0-9a-fA-F]){2}[-:]){5}([0-9a-fA-F]){2})")
+        val m = pat.matcher(newMac)
+
+        if (m.find()) {
+            val result = m.group()
+            return result.split("(\\:|\\-)".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        } else {
+            throw IllegalArgumentException("Invalid MAC address")
+        }
+    }
+
+}
